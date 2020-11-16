28f33c1f93a48f0cb2615abe51f9a527ebdba754
Branca Chiotte <brancachiotte@gmail.com>
2020-11-16 17:38:17 +0000

    fixing bigs

--- app.js
+++ app.js
@@ -157,25 +157,25 @@ var linkRegister = document.querySelector('.register-thru-login');
 linkRegister.addEventListener('click', () => startModal('register-container'));
 
 
-
-function storeUsers(){
-    var name = document.getElementById('name');
+// save user and password
+function store(){
+    var email = document.getElementById('email');
     var password = document.getElementById('password');
     var lowerCaseLetters = /[a-z]/g;
     var upperCaseLetters = /[A-Z]/g;
     var numbers = /[0-9]/g;
 
-    if(name.value.length == 0){
+    if(email.value.length == 0){
         alert('Please fill in email');
 
     }else if(password.value.length == 0){
         alert('Please fill in password');
 
-    }else if(name.value.length == 0 && password.value.length == 0){
+    }else if(email.value.length == 0 && password.value.length == 0){
         alert('Please fill in email and password');
 
-    }else if(password.value.length > 8){
-        alert('Max of 8');
+    }else if(password.value.length < 5){
+        alert('Password need to have more than 5 characters');
 
     }else if(!password.value.match(numbers)){
         alert('please add 1 number');
@@ -187,7 +187,7 @@ function storeUsers(){
         alert('please add 1 lovercase letter');
 
     }else{
-        localStorage.setItem('name', name.value);
+        localStorage.setItem('email', email.value);
         localStorage.setItem('password', password.value);
         alert('Your account has been created');
     }
@@ -195,17 +195,16 @@ function storeUsers(){
 
 //checking
 function check(){
-    var storedName = localStorage.getItem('name');
+    var storedEmail = localStorage.getItem('email');
     var storedPassword = localStorage.getItem('password');
 
-    var userName = document.getElementById('userName');
+    var userEmail = document.getElementById('userEmail');
     var userPassword = document.getElementById('userPassword');
     var userRemember = document.getElementById("rememberMe");
 
-    if(userName.value == storedName && userPassword.value == storedPassword){
+    if(userEmail.value == storedEmail && userPassword.value == storedPassword ){
         alert('You are logged in.');
     } else{
-      alert('Error on login.');
+        alert('Error on login');
     }
 }
-
